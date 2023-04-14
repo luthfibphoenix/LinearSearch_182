3bf289b1b8d600c7b49d9591dc43d10ba2700b6b
luthfibphoenix <lutfibahtiar2409@gmail.com>
2023-04-14 10:39:30 +0700

    rumus input

--- LinearSearch_182/LinearSearch_182.cpp
+++ LinearSearch_182/LinearSearch_182.cpp
@@ -9,5 +9,15 @@ int i; //Index of array element
 
 void input()
 {
+	while (true)
+	{
+		cout << "Enter the number of elements in the array : ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray should have minimum 1 and maximum 20 elements' \n\n";
+	}
+
 
 }
